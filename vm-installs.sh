b6545e2363919aac2bd0602b8bd408b555fd3083
Elizabeth McDaniel <emcdaniel@wisc.edu>
2017-07-13 13:05:32 -0500

    More installations specific to working on anvi'o up and running

--- vm-installs.sh
+++ vm-installs.sh
@@ -48,22 +48,67 @@ gzip -d BBMap_37.36.tar.gz
 tar xvz BBMap_37.36.tar
 rm BBMap_37.36.tar
 
+
 # hmmer
 sudo wget http://eddylab.org/software/hmmer3/3.1b2/hmmer-3.1b2-linux-intel-x86_64.tar.gz
 tar -xvzf hmmer-3.1b2-linux-intel-x86_64.tar.gz
 rm -xvzf hmmer-3.1b2-linux-intel-x86_64.tar.gz
 cd hmmer-3.1b2-linux-intel-x86_64
 ./configure && make && sudo make install
+cd ..
 
 # MCL
 sudo wget http://www.micans.org/mcl/src/mcl-latest.tar.gz
 tar -zxvf mcl-14-137.tar.gz && cd mcl-14-137
 ./configure && make && sudo make install
+cd ..
 
 # For installing blast, ftp 2.6.0 over with cyberduck? I'm honestly not sure how I got it to work. 
 
+# Things for anvi'o 
 
-# Python packages - specifically pandas
+# Prodigal
+wget https://github.com/hyattpd/Prodigal/archive/v2.6.2.tar.gz
+tar -zxvf v2.6.2.tar.gz && cd Prodigal-2.6.2/ && make
+sudo cp prodigal /usr/local/bin/
+cd ..
+rm v2.6.2.tar.gz
+
+# SQLite 3
+sudo apt-get install sqlite3
+
+# GNU Scientific Library
+
+wget ftp://ftp.gnu.org/gnu/gsl/gsl-latest.tar.gz
+tar -zxvf gsl-latest.tar.gz
+cd gsl-*
+./configure && make && sudo make install
+cd ..
+rm gsl-latest.tar.gz
+
+# Cython
+sudo pip3 install Cython
+
+# Fasttree
+
+wget http://www.microbesonline.org/fasttree/FastTree.c
+gcc -DNO_SSE -O3 -finline-functions -funroll-loops -Wall -o FastTree FastTree.c -lm
+sudo mv FastTree /usr/local/bin
+rm FastTree.c
+
+# HDF5
+
+wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8/hdf5-1.8.17/src/hdf5-1.8.17.tar.bz2
+tar -jxvf hdf5-1.8.17.tar.bz2
+cd hdf5-1.8.17
+./configure && make && sudo make install
+cd ..
+rm hdf5-1.8.17.tar.bz2
+
+# Did not install centrifuge, eggnog, or muscle for now. Should be fine. Although I might need muscle down the line for some things. For now I will install anvi'o codebase later on. 
+
+
+# Python packages - specifically pandas, also installs numpy
 cd /home
 # First need to install pip
 sudo apt-get install python3-pip
